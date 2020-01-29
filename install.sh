e4ce03a459fd7f9298814803f3c4028f9617799a
eleutherius <eleutherius69@gmail.com>
2020-01-29 22:44:24 +0200

    add installer

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+base_python_interpreter=""
+project_path=`pwd`
+
+read -p "Python interpreter: " base_python_interpreter
+read -p "Your domain without protocol (for example, google.com): " project_domain
+`$base_python_interpreter -m venv env`
+source env/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+
+sed -i "pwm_manage_path~$project_path~g" systemd/pwm_manage.service
+
+sudo ln -s $project_path/systemd/pwm_manage.service /etc/systemd/system/
+
+sudo systemctl daemon-reload
+sudo systemctl start pwm_manage
+sudo systemctl enable pwm_manage
