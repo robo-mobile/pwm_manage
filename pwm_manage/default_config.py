c7f987908017dd8d59ba95012c0c9ebcf311779b
eleutherius <eleutherius69@gmail.com>
2020-10-04 16:58:03 +0300

    fix default config

--- pwm_manage/default_config.py
+++ pwm_manage/default_config.py
@@ -4,21 +4,39 @@ title = "PWM manager config"
 
 pwm_type = "standart"           # choose  one of: standart/L292N/L292
 
-[outputs_l298n]
-channel1 = 36
-channel2 = 37
-channel3 = 38
-channel4 = 39
 
-[outputs_l298n_double]
-channel1 = 36
-channel2 = 37
-channel3 = 38
-channel4 = 39
+# standat pwm engine type
 
-[outputs_standart]
-channel1 = 36
-channel2 = 37
-channel3 = 38
-channel4 = 39
+# [outputs]
+# channel1 = 36
+# channel2 = 37
+# channel3 = 38
+# channel4 = 39
+
+# l298n engine type
+
+# [outputs]
+# enA = 35
+# channel1 = 36
+# channel2 = 37
+# enB = 40
+# channel3 = 38
+# channel4 = 39
+
+# double l298n engine type
+
+# [outputs]
+# enA1 = 35
+# channel11 = 36
+# channel12 = 37
+# enB1 = 40
+# channel13 = 38
+# channel14 = 39
+# enA2 = 35
+# channel21 = 36
+# channel22 = 37
+# enB2 = 40
+# channel23 = 38
+# channel24 = 39
 '''
+
